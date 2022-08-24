75346340c93390e9b029a8874c55ba122b08177c
yuinakorn@hotmail.com <yuinakorn@hotmail.com>
2022-08-25 01:04:42 +0700

    update step2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let datetime = moment().format('YYYY-MM-DD HH:mm:ss');
 const backup_dir = CUR_DIR + '/backup';
 const table_list = CUR_DIR + '/tablelist.txt';
 
-console.log('[1/5] start process at ' + datetime);
+console.log('[1/5] ' + datetime + ' start processing');
 
 fs.readFile(table_list, 'utf8', function (err, data) {
     if (err) throw err;
@@ -37,6 +37,11 @@ fs.readFile(table_list, 'utf8', function (err, data) {
     exec(cmd, function (error) {
         if (error !== null) {
             console.log('exec error: ' + error);
+            let log = datetime + ' mysql dump error' + "\r\n";
+            fs.appendFile(backup_dir + '/log.txt', log, function (err) {
+                    if (err) throw err;
+                }
+            );
         }
         compress(backup_file);
     });
@@ -50,8 +55,8 @@ function compress(backup_file) {
             console.log('exec error: ' + error);
         }
         datetime = moment().format('YYYY-MM-DD HH:mm:ss');
-        console.log('[2/5] backup && compress completed at ' + datetime);
-        console.log('[3/5] log saved');
+        console.log('[2/5] ' + datetime + ' backup && compress completed');
+        console.log('[3/5] ' + datetime + ' log saved');
         write_log();
         let backup_file_gz = DB_NAME + '.sql.gz';
         upload(backup_file_gz);
@@ -64,14 +69,14 @@ function upload(backup_file_gz) {
     let cmd = 'sshpass -p \"' + ROMOTE_PASSWORD + '\" scp -P ' + SSH_PORT + ' ' + backup_dir + '/' + backup_file_gz
         + ' ' + REMOTE_USER + ':/var/backup/';
     datetime = moment().format('YYYY-MM-DD HH:mm:ss');
-    console.log('[4/5] start upload at ' + datetime);
+    console.log('[4/5] ' + datetime + ' start upload');
 
     exec(cmd, function (error) {
             if (error !== null) {
                 console.log('exec error: ' + error);
             } else {
                 datetime = moment().format('YYYY-MM-DD HH:mm:ss');
-                console.log('[5/5] upload completed at ' + datetime);
+                console.log('[5/5] ' + datetime + ' upload completed');
             }
 
         }
@@ -82,7 +87,7 @@ function upload(backup_file_gz) {
 function write_log() {
     //    write log file
     datetime = moment().format('YYYY-MM-DD HH:mm:ss');
-    let log = 'backup success at ' + datetime + "\r\n";
+    let log = datetime + ' backup completed' + "\r\n";
     fs.appendFile(backup_dir + '/log.txt', log, function (err) {
             if (err) throw err;
         }
